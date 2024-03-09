d9164539cf3e7f92f1724bf7d8a9c65f66813581
negar <negar.karimnejad@gmail.com>
2024-03-09 22:45:02 +0330

    coding

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,8 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  let { data: cabins, error } = await supabase.from("cabins").select("*");
-  console.log(cabins);
-  console.log(error);
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) throw new Error(error.message);
+  return data;
 }
